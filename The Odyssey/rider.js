40dc2095da577587a3f8c642d7ff0c7b97ffc12d
GojoSatoru1738 <ajmiller@email.neit.edu>
2025-12-04 09:12:11 -0500

    MY MAMAS KINDA HOMELESS

--- The Odyssey/rider.js
+++ The Odyssey/rider.js
@@ -9,33 +9,19 @@ export class Rider {
         this.gravity = 1;
         this.jumpStrength = -15;
 
-        // Event listener for jump
-        window.addEventListener("keydown", (e) => {
-            if (e.code === "Space" && this.onGround) {
-                this.ySpeed = this.jumpStrength;
-            }
-        });
+        this.width = 20;
+        this.height = 20;
 
         this.onGround = false;
     }
 
-    update(terrain) {
+    update() {
         this.ySpeed += this.gravity;
         this.y += this.ySpeed;
-
-        const groundY = terrain.groundYAt(this.x);
-
-        if (this.y >= groundY - 40) { 
-            this.y = groundY - 40;
-            this.ySpeed = 0;
-            this.onGround = true;
-        } else {
-            this.onGround = false;
-        }
     }
 
     draw() {
         this.pencil.fillStyle = "red";
-        this.pencil.fillRect(this.x - 10, this.y - 20, 20, 20);
+        this.pencil.fillRect(this.x - this.width/2, this.y - this.height, this.width, this.height);
     }
 }
